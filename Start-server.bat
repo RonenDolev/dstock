e3e773e8834a142a92c08116b5299581676208f9
RonenDolev <ronen.dolev@gmail.com>
2025-07-22 10:17:59 +0300

    Add files via upload

--- Start-server.bat
+++ Start-server.bat
@@ -1,4 +1,9 @@
 @echo off
 cd /d C:\dstock
 call npm install
+
+REM פותח את הדפדפן אחרי 3 שניות, במקביל
+start "" cmd /c "timeout /t 3 >nul && start http://localhost:3000"
+
 call npm run dev
+pause
